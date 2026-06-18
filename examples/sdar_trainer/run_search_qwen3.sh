b24a218947ff5f730f5c7c65748e396804f8d39f
asayu255 <asayu255@gmail.com>
2026-06-18 19:23:17 +0900

    update

--- examples/sdar_trainer/run_search_qwen3.sh
+++ examples/sdar_trainer/run_search_qwen3.sh
@@ -1,7 +1,7 @@
 set -x
 ENGINE=${1:-vllm}
 
-export WANDB_API_KEY=your_key_here
+export WANDB_API_KEY=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
 export HIGHLIGHT_CONFIGS='<search>:0,0,255;</search>:0,0,255;<information>:255,0,0;</information>:255,0,0'
 
 # SDAR hyperparameters
@@ -11,8 +11,8 @@ skill_all=false
 
 experiment_name="sdar_qwen3_1.7b_coef${sdar_coef}_beta${gate_beta}_skillall${skill_all}"
 
-train_data_size=16
-val_data_size=128
+train_data_size=15
+val_data_size=126
 group_size=8
 
 TRAIN_DATA="$HOME/data/searchR1_processed_direct/train.parquet"
@@ -34,22 +34,22 @@ python3 -m verl.trainer.main_sdar \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.1 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=60 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=10 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.enable_chunked_prefill=False \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=False \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.use_invalid_action_penalty=True \
     actor_rollout_ref.actor.invalid_action_penalty_coef=0.01 \
